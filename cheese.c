817ffa07f8702b9ba6c46bd02249edca27e27f9c
Samuel Berkun <sberkun@berkeley.edu>
2022-09-12 21:50:41 +0000

    better lock impl, sema impl

--- cheese.c
+++ cheese.c
@@ -1,25 +1,27 @@
 
 #include <stdio.h>
 
-
+/* A lock is a __uint32_t that is set to 0 for unlocked, and 1 for locked */
 void low_lock_aquire(volatile __uint32_t* lock) {
     __sync_synchronize();
-    asm (
-        "li t0, 1\n"
-        "1:\n"
-        "amoswap.w.aq t0, t0, (%0)\n"
-        "bnez t0, 1b"
-        : // outputs
-        : "r"(lock) //inputs
-        : "t0" // clobbers
-    );
+    __uint32_t x = 1;
+    do {
+        asm volatile (
+          "amoswap.w.aq %0, %1, (%2)" // aq = aquire sematics
+          : "=r" (x) // outputs
+          : "0"(x), "r"(lock) //inputs
+          // "0" means will use same register for input x and output x 
+          : // clobbers
+        );
+    } while (x != 0);
     __sync_synchronize();
 }
 
+/* A lock is a __uint32_t that is set to 0 for unlocked, and 1 for locked */
 void low_lock_release(volatile __uint32_t* lock) {
     __sync_synchronize();
-    asm (
-        "amoswap.w.rl x0, x0, (%0)"
+    asm volatile (
+        "amoswap.w.rl x0, x0, (%0)" // rl = release sematics
         : // outputs
         : "r"(lock) //inputs
         : // clobbers
@@ -27,9 +29,50 @@ void low_lock_release(volatile __uint32_t* lock) {
     __sync_synchronize();
 }
 
+/* A semaphore is a __uint32_t that represents the counter */
+void low_sema_down(volatile __uint32_t* sema) {
+    __sync_synchronize();
+    __uint32_t x = 0;
+    do {
+        asm volatile (
+          "amoswap.w.aq %0, x0, (%1)" // aq = aquire sematics
+          : "=r"(x) // outputs
+          : "r"(sema) //inputs 
+          : // clobbers
+        );
+    } while (x == 0);
 
-volatile __uint32_t main_lock;
+    // x represents the value of the counter before we set it to 0
+    // want to add x-1 back to the counter
+    x -= 1;
+    asm volatile (
+        "amoadd.w.rl x0, %0, (%1)" // rl = release sematics (releasing x-1)
+        : // outputs
+        : "r"(x), "r"(sema) //inputs
+        : // clobbers
+    );
+    __sync_synchronize();
+}
+
+/* A semaphore is a __uint32_t that represents the counter */
+void low_sema_up(volatile __uint32_t* sema) {
+    __sync_synchronize();
+    asm volatile (
+        "li t0, 1\n"
+        "amoadd.w.rl x0, t0, (%0)" // rl = release sematics
+        : // outputs
+        : "r"(sema) //inputs
+        : "t0" // clobbers
+    );
+    __sync_synchronize();
+}
+
+
+
+volatile __uint32_t main_lock = 0;
+volatile __int32_t main_sema = 0;
 volatile int cheese = 0;
+int NUM_THREADS_TO_WAIT_FOR = 8;
 
 void thread_entry(int cid, int nc)
 {
@@ -43,22 +86,20 @@ void thread_entry(int cid, int nc)
   cheese = banana + 1;
   low_lock_release(&main_lock);
   
+  low_sema_up(&main_sema);
 
   while (cid != 0);
 }
 
-int fib(int n) {
-  if (n < 2) {
-    return n;
-  }
-  return fib(n-1) + fib(n-2);
-}
-
 int main() {
+    // wait for all threads to complete
+    for (int a = 0; a < NUM_THREADS_TO_WAIT_FOR; a++) {
+      low_sema_down(&main_sema);
+    }
     
-    int fib_result = fib(30);
     low_lock_aquire(&main_lock);
-    printf("I like cheese too %d %d\n", cheese, fib_result);
+    printf("I like cheese too! value of cheese: %d\n", cheese);
+    printf("Value of sema: %d\n", main_sema);
     low_lock_release(&main_lock);
     return 0;
 }
